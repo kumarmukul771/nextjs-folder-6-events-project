3e02e086fc052b5e70a23fab56bc9af0b7aa96fb
kumarmukul771 <kumarmukul771@gmail.com>
2021-04-02 14:00:17 +0530

    pre-rendering done by own method and data fetched from firebase as backend

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,44 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
+import { useEffect, useState } from "react";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+  const [featuredEvents, setFeaturedEvents] = useState(props.featuredEvents);
+
+  useEffect(async () => {
+    const response = await fetch(
+      "https://netjs-course-default-rtdb.firebaseio.com/events.json"
+    );
+    const data = await response.json();
+    let updatedEvents = [];
+    for (let key in data) {
+      updatedEvents.push({ ...data[key], id: key });
+    }
+
+    setFeaturedEvents(updatedEvents);
+  }, []);
 
   return (
     <div>
       <EventList items={featuredEvents} />
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const response = await fetch(
+    "https://netjs-course-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+  let updatedEvents = [];
+  for (let key in data) {
+    updatedEvents.push({ ...data[key], id: key });
+  }
+  return {
+    props: {
+      featuredEvents: updatedEvents,
+    },
+  };
 }
 
 export default HomePage;
